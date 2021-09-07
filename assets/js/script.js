bae8de94dc9b46aaecc35f1bdd12f53c45131e4d
Rod1GitHub <rz@contacto1.com>
2021-09-07 17:44:33 -0300

    Commit versión 1.0 terminada

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//Tooltip for Bootstrap v5.1 - Enable tooltips everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+//Popovers for Bootstrap v5.1
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+//Popovers - Using the container option
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+    container: 'body'
+  })
